6f2d1ad5a275097acc80586f75bff65056e0c6b1
Matheus Chong <matheus.chong@gmail.com>
2022-01-29 10:49:50 -0300

    Adicionado botão fica verde ao selecionar 3 itens

--- script/script.js
+++ script/script.js
@@ -6,4 +6,28 @@ function selecionarAlimento(categoriaDaOpçao,elemento){
 
     elemento.classList.toggle("selecionado")
 
+    verifica3Selecionados() 
+}
+
+function verifica3Selecionados(){
+    let btn=document.querySelector(".fechar-pedido__btn")
+
+    if(btn.innerHTML == "<p>Fechar pedido</p>"){
+        return;
+    }
+
+    let prato=document.querySelector(".categoria__opcoes-pratos .selecionado")
+    let bebida=document.querySelector(".categoria__opcoes-bebidas .selecionado")
+    let sobremesa=document.querySelector(".categoria__opcoes-sobremesas .selecionado")
+
+    if( ((prato && bebida)&&sobremesa) !== null){
+        
+        btn.innerHTML=("<p>Fechar pedido</p>")
+
+        btn.classList.add("btn-verde")
+    }
+}
+
+function fecharPedido(){
+    
 }
